1293ed33d7a764c45130345265eda3ad89abcc2e
jmejia32 <javier_junior3@hotmail.com>
2015-11-23 02:20:51 -0500

    Agregar Capacitacion y Trabajador

---
+++ forms.py
@@ -0,0 +1,57 @@
+#encoding:utf-8
+from django import forms
+from django.forms import ModelForm
+from capacit.models import *
+
+class DateInput(forms.DateInput):
+	input_type = "date"
+
+
+class TrabajadorForm(ModelForm):
+	class Meta:
+		model = Trabajador
+		fields = ('cc','nombre','apellido')
+		labels = {
+			'cc': 'Cédula de Ciudadanía',
+			'nombre': 'Nombre',
+			'apellido': 'Apellido'
+		}
+		widgets = {
+			'cc': forms.TextInput(attrs={'class': 'form-control'}),
+			'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+			'apellido': forms.TextInput(attrs={'class': 'form-control'})	
+		}
+
+
+class CapacitacionForm(ModelForm):
+	class Meta:
+		model = Capacitacion
+		fields = ('nombre', 'descrip', 'fechaI','fechaF','tutor')
+		labels = {
+			'nombre':'Nombre de la Capacitación', 
+			'descrip':'Descripción', 
+			'fechaI':'Fecha de Inicio',
+			'fechaF':'Fecha de Finalización',
+			'tutor':'Nombre del Tutor'
+		}
+		widgets = {
+			'nombre': forms.TextInput(attrs={'class': 'form-control'}), 
+			'descrip': forms.TextInput(attrs={'class': 'form-control'}), 
+			'fechaI': DateInput(attrs={'class': 'form-control'}),
+			'fechaF': DateInput(attrs={'class': 'form-control'}),
+			'tutor': forms.TextInput(attrs={'class': 'form-control'})
+		}
+
+
+class AsignacionForm(ModelForm):
+	class Meta:
+		model = Asignacion
+		fields = ('idTrab','idCapacit')
+		labels = {
+			'idTrab':'Seleccione un Trabajador',
+			'idCapacit':'Seleccione una Capacitación'
+		}
+		widgets = {
+			'idTrab': forms.Select(attrs={'class': 'form-control'})
+			'idCapacit': forms.Select(attrs={'class': 'form-control'})
+		}
--- views.py
+++ views.py
@@ -1,7 +1,9 @@
+#encoding:utf-8
 from django.shortcuts import render
 from django.template import loader, Context
 from django.http import HttpResponse
 from capacit.models import *
+from capacit.forms import *
 # Create your views here.
 
 def inicio(request):
@@ -13,14 +15,20 @@ def modifcapacit(request):
 
 
 def agrcapacit(request):
-	pass
+	if request.method == 'POST':
+		formulario = CapacitacionForm(request.POST)
+		if formulario.is_valid():
+			formulario.save()
+			return render(request, "ejemplo.html", {'msj':'La capacitación se registró correctamente'})
+		else:
+			formulario = CapacitacionForm()
+			return render(request, "agrcapacit.html", {'err':'Complete todos los campos', 'formulario':formulario})
+	else:
+		formulario = CapacitacionForm()
+		return render(request, "agrcapacit.html",{'formulario':formulario})
 
 
 def elimcapacit(request):
-	pass
-
-
-def salir(request):
 	pass
 
 
@@ -38,3 +46,17 @@ def login(request):
 
 def salir(request):
 	pass
+
+
+def agrtrab(request):
+	if request.method == 'POST':
+		formulario = TrabajadorForm(request.POST)
+		if formulario.is_valid():
+			formulario.save()
+			return render(request, "ejemplo.html", {'msj':'El trabajador se ha guardado correctamente'})
+		else:
+			formulario = TrabajadorForm()
+			return render(request, "agrtrab.html", {'err':'Complete todos los campos', 'formulario':formulario})
+	else:
+		formulario = TrabajadorForm()
+		return render(request, "agrtrab.html", {'formulario':formulario})
